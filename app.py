db42aaf46cdcdbf3d90d6bded42e11c9f3459abd
Addaian <85748975+Addaian@users.noreply.github.com>
2021-09-27 11:15:40 +0100

    mm

--- app.py
+++ app.py
@@ -1,5 +1,46 @@
 from flask import Flask, render_template
 
+
+class Users:
+    def __init__(self, fname, lname, email, password):
+        self.fname = fname
+        self.lname = lname
+        self.email = email
+        self.password = password
+
+    def email_func(self, email):
+        check1 = 0
+        check2 = 0
+        a = [self.email.split()]
+        for x in a:
+            if a[x] == '@' and check1 == 0:
+                check1 = 1
+            if a[x] == '.' and check1 == 1:
+                check2 = 1
+        if check2 == 1:
+            return "correct"
+        if check2 == 0:
+            return "change password"
+
+    def password_func(self, password):
+        if 6 <= len(self.password) <= 12:
+            a = (int(s) for s in password().split())
+            for x in len(a):
+                if 97 <= ord(a[x]) <= 122:
+                    check1 = 1
+                if 65 <= ord(a[x]) <= 90:
+                    check2 = 1
+            if check1 and check2 == 1:
+                print("true")
+            else:
+                print("false")
+
+
+myUser = Users("adrian", "dai", "adandian3@gmail.com", "12345")
+print(myUser.fname, myUser.lname)
+myUser.email_func("adandian3@gmail.com")
+print(myUser.fname, myUser.lname)
+
 app = Flask(__name__)
 
 
@@ -10,37 +51,3 @@ def home():  # put application's code here
 
 if __name__ == '__main__':
     app.run()
-    class Users:
-        def __init__(self, name, email, password):
-            self.name = name
-            self.email = email
-            self.email = password
-
-        def function(email):
-            check1 = 0
-            a = (int(s) for s in email.split())
-            for x in len(a):
-                if a[x] == '@' and check1 == 0:
-                   check1 = 1
-                if a[x] == '.' and check1 ==1:
-                    check2 = 1
-            if check2 == 1:
-                print("true")
-            if check2 == 0:
-                print("false")
-
-        def function(password):
-            if len(password) >= 6 and len(password) <= 12:
-                a = (int(s) for s in password().split())
-                for x in len(a):
-                    if 97 <= ord(a[x]) <= 122:
-                        check1 = 1
-                    if 65 <= ord(a[x]) <= 90:
-                        check2 = 1
-                if check1 and check2 == 1:
-                    print("true")
-                else:
-                    print("false")
-
-
-
